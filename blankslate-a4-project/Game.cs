f0fed65fc0f9f23652948adaf6e6b9cad8096058
PYNIERRE <zachary.laflamme@mohawkcollege.ca>
2025-11-21 23:30:32 -0500

    ACTUALLY successfully ported to main. use this version and other versions thereafter

--- blankslate-a4-project/Game.cs
+++ blankslate-a4-project/Game.cs
@@ -11,16 +11,24 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
+        Player player = new Player();
+        Water water = new Water();
 
+        public int floorLevel; // the position of the floor relative to the bottom of the screen
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
-            Window.SetTitle("Dorlphin");
-            Window.SetSize(1000, 600);
-            Window.TargetFPS = 60;
+            Window.SetTitle("Dolph");
+            Window.SetSize(1200, 700);
+            Window.TargetFPS = 120;
+
+            player.position = player.startPosition;
+            water.waterLevelTarget = 400;
+            water.waterLevel = 50;
+            floorLevel = 75;
         }
 
         /// <summary>
@@ -30,6 +38,21 @@ namespace MohawkGame2D
         {
             Window.ClearBackground(Color.White);
 
+            DebugVisuals(player); // putting values into visuals
+
+            water.Update();
+            player.Update();
+        }
+
+        void DebugVisuals(Player player)
+        {
+            Text.Size = 14;
+
+            Text.Draw($"pressure: {player.pressure}", 20, 100);
+            Text.Draw($"acceleration: {player.acceleration}", 20, 80);
+            Text.Draw($"maxspeed: {player.maxSpeed}", 20, 60);
+            Text.Draw($"position: {player.position}", 20, 40);
+            Text.Draw($"velocity: {player.velocity}", 20, 20);
         }
     }
 
---
+++ blankslate-a4-project/Water.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohawkGame2D
+{
+    public class Water
+    {
+        public float waterLevel; // the position of the floor relative to the bottom of the screen
+        public int waterLevelTarget; // target that the water level tweens to
+        public void Setup()
+        {
+
+        }
+        public void Update()
+        {
+            WaterLevelMovement();
+            FloatingPointFix();
+
+            DrawWater();
+            WaterTest(); // debug for water
+        }
+        void WaterLevelMovement()
+        {
+            waterLevel = ((25 * waterLevel) + waterLevelTarget) / 26; // slowly ease waterLevel to waterLevelTarget
+        }
+        void FloatingPointFix()
+        {
+            if (waterLevel < waterLevelTarget + 1 && waterLevel > waterLevelTarget - 1) waterLevel = waterLevelTarget; // ik its such a small line of code but i wanted to keep it like this for organization sake just like the player cs
+        }
+        void DrawWater()
+        {
+            Draw.LineSize = 1;
+            Draw.LineColor = Color.Black;
+            Draw.FillColor = Color.Gray;
+            Draw.Rectangle(0, Window.Height - (int)waterLevel, Window.Width, Window.Height * 2);
+        }
+
+        void WaterTest() // module to change the "water level target" to a random number
+        {
+            Text.Draw($"waterLevel: {waterLevel}", 20, 140);
+            Text.Draw($"waterLevelTarget: {waterLevelTarget}", 20, 160); // hastily put debug visuals into here
+            bool waterDebug = (Input.IsKeyboardKeyPressed(KeyboardInput.E));
+            if (waterDebug) waterLevelTarget = Random.Integer(250, 600);
+        }
+    }
+}
