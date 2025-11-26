33c29220ded91d58a46807bdd1583351b8280ed1
EwanDahlgren2007 <ewan.a.dahlgren@gmail.com>
2025-11-26 15:27:49 -0500

    Obstacle.cs actually added.

---
+++ blankslate-a4-project/Obstacle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohawkGame2D
+{
+    public class Obstacle
+    {
+        public void Setup()
+        {
+
+        }
+
+        public void Update()
+        {
+            CollideObstacle();
+            DrawObstacle();
+        }
+
+        // detects obstacles colliding with things
+        void CollideObstacle()
+        {
+
+        }
+
+        // draws obstacles
+        void DrawObstacle()
+        {
+
+        }
+
+        // if an obstacle is past the player, delete it
+        void CullObstacle()
+        {
+
+        }
+    }
+}
