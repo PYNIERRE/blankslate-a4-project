a6b255d2981a79a05c5149bd6c78aba318f782bd
EwanDahlgren2007 <ewan.a.dahlgren@gmail.com>
2025-11-27 11:32:36 -0500

    it technically works without issue. that being said, it can't render anything yet taking 5 to eat something, will continue soon after

--- blankslate-a4-project/Obstacle.cs
+++ blankslate-a4-project/Obstacle.cs
@@ -8,15 +8,15 @@ namespace MohawkGame2D
 {
     public class Obstacle
     {
-        // all of the arrays the functions will access
+        // all of the arrays the functions will access, would use vector2, but that'd take up way more lines than just four separate arrays
 
-        float[] obstacleColliderX1Positions = []; // always set this value to 0, unless the image doesn't line up perfectly with the hitbox
-        float[] obstacleColliderY1Positions = []; // set this value to 0 if the obstacle starts at the ceiling
-        float[] obstacleColliderX2Positions = []; // never set this value to 0, as the collider would simply be a line
-        float[] obstacleColliderY2Positions = []; // same goes for this one
-        float[] obstacleOffsets = []; // set this value to 0 if you want it to start all the way on the left of the screen
-        float[] obstacleImageYValues = []; // only needs a y value, as it's position is tied to the obstacle offset
-        Texture2D[] obstacleImages = []; // all images go here
+        float[] colliderX1Positions = []; // always set this value to 0, unless the image doesn't line up perfectly with the hitbox
+        float[] colliderY1Positions = []; // set this value to 0 if the obstacle starts at the ceiling
+        float[] colliderX2Positions = []; // never set this value to 0, as the collider would simply be a line
+        float[] colliderY2Positions = []; // same goes for this one
+        float[] offsets = []; // set this value to 0 if you want it to start all the way on the left of the screen
+        float[] imageYValues = []; // only needs a y value, as it's position is tied to the obstacle offset
+        Texture2D[] images = []; // all images go here
         public void Setup()
         {
 
@@ -24,21 +24,26 @@ namespace MohawkGame2D
 
         public void Update()
         {
-            for(int i = 0; i < (obstacleColliderX1Positions.Length); i++)
+            // cycles through each defined obstacle
+            for(int i = 0; i < (colliderX1Positions.Length); i++)
             {
-                CollideObstacle();
-                DrawObstacle();
+                // if the obstacle is undefined, don't try to render it
+                if(images[i] != null)
+                {
+                    CollideObstacle(colliderX1Positions[i], colliderY1Positions[i], colliderX2Positions[i], colliderY2Positions[i], offsets[i]);
+                    DrawObstacle(imageYValues[i], images[i], offsets[i], i);
+                }
             }
         }
 
         // detects obstacles colliding with things
-        void CollideObstacle(float colliderAreaX1, float colliderAreaY1, float colliderAreaX2, float colliderAreaY2, float obstacleOffset, int obstacleNumber)
+        void CollideObstacle(float colliderAreaX1, float colliderAreaY1, float colliderAreaX2, float colliderAreaY2, float obstacleOffset)
         {
             
         }
 
         // draws obstacles
-        void DrawObstacle(float obstacleImageY, Texture2D obstacleImage, float obstacleOffset, int obstacleNumber)
+        void DrawObstacle(float obstacleImageY, Texture2D obstacleImage, float obstacleOffset, int index)
         {
 
 
@@ -46,7 +51,7 @@ namespace MohawkGame2D
 
             if (obstacleOffset < 0)
             {
-                CullObstacle();
+                CullObstacle(index);
             }
         }
         // if an obstacle is past the player, delete it
